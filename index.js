172288e1fc036a2b2377ee4bbf3de3648273aaa7
rifat-islam2026 <rifat.info001@gmail.com>
2024-10-26 00:36:38 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,65 @@
+const express = require('express')
+const app = express()
+require('dotenv').config()
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const cors = require('cors')
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.USER_DB}:${process.env.USER_PASS}@cluster0.kkyxc.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+
+async function run() {
+    try {
+        // Connect the client to the server	(optional starting in v4.7)
+        await client.connect();
+
+        const userCollection = client.db("userDB").collection("user");
+
+        app.post('/users', async (req, res) => {
+            const users = req.body;
+            console.log(users);
+            const result = await userCollection.insertOne(users);
+            res.send(result);
+        })
+        
+        app.get('/users', async (req, res) =>{ 
+            const cursor = await userCollection.find().toArray();
+            res.send(cursor);
+        })
+
+        app.delete('/users/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id)};
+            const result = await userCollection.deleteOne(query);
+            res.send(result);
+        })
+
+        // Send a ping to confirm a successful connection
+        await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+        // Ensures that the client will close when you finish/error
+        // await client.close();
+    }
+}
+run().catch(console.dir);
+
+
+app.get('/', (req, res) => {
+    res.send('USER MANAGEMENT SYSTEM SERVER IS RUNNING!')
+})
+
+app.listen(port, () => {
+    console.log(`User Management System Server On Port ${port}`)
+})
